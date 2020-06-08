4a2a5ab5d77a1d7aa100d71e3cc4d8e8bf6f619a
Dan Reardon <reardon.danielj@gmail.com>
2020-06-08 11:13:44 -0400

    week 1 algorithmic toolbox complete

---
+++ algorithmic-toolbox/maximum_pairwise_product/max_pairwise_product.py
@@ -0,0 +1,13 @@
+# python3
+
+
+# Not the most optimal solution, but the most compact
+def max_pairwise_product(numbers):
+    numbers.sort(reverse=True)
+    return numbers[0] * numbers[1]
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    input_numbers = [int(x) for x in input().split()]
+    print(max_pairwise_product(input_numbers))
